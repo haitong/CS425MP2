485f02e698fe15787f05cbbe550ab14888c91d77
Mengjia Yan <mengjiayan0720@gmail.com>
2015-04-12 23:03:24 -0500

    update test.py

--- bin/test.py
+++ bin/test.py
@@ -1,9 +1,9 @@
 import os;
 
-node_num = [4, 8, 12, 20, 30, 40, 100];
+node_num = [4, 8, 12, 20, 30, 40, 60, 80, 100, 150, 200];
 
 for node in node_num:
-    for i in range(1):
+    for i in range(5):
         os.system("java Test "+`node`+" 1000 test."+`node`+".1000 "+`i`);
 
 
